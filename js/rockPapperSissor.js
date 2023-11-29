86671bd9bd8a633a1a46f7decd735323f40da85a
Jens Bengtsson <jb225je@student.lnu.se>
2023-11-29 13:04:57 +0100

    html, css for rock, paper, sissor

--- js/rockPapperSissor.js
+++ js/rockPapperSissor.js
@@ -1,8 +1,28 @@
 const gameArray = ['rock', 'paper', 'sissor'];
+const rock = document.getElementById('rock');
+const paper = document.getElementById('paper');
+const sissor = document.getElementById('sissor');
+const options = [rock, paper, sissor];
+
+let computersWepon = 0;
 
 const newrockPapperSissorGame = () => {
     const rockPapperSissorContainer = document.querySelector(".rockPapperSissor");
     rockPapperSissorContainer.classList.remove('displayContainer');
+    options.forEach(option => {
+        option.addEventListener("click", userchoies)
+    });
 }
 
-// test
+function userchoies() {
+    computersChoies();
+}
+
+function computersChoies() {
+    computersWepon = Math.floor(Math.random() * 2);
+    console.log(computersWepon);
+}
+
+
+
+// event.stopPropagation(); // gör att det inte 'bubblar' upp till parent-element
